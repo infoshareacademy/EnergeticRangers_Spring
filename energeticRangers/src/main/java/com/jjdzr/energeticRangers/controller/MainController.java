102aed9dff3f265b7f570709e2641c9b1613576f
Katarzyna Kulas <kulas.kat@gmail.com>
2023-06-18 17:10:44 +0200

    last commit2

--- energeticRangers/src/main/java/com/jjdzr/energeticRangers/controller/MainController.java
+++ energeticRangers/src/main/java/com/jjdzr/energeticRangers/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "userLogin";
     }
 }
--- energeticRangers/src/main/java/com/jjdzr/energeticRangers/service/UserService.java
+++ energeticRangers/src/main/java/com/jjdzr/energeticRangers/service/UserService.java
@@ -6,4 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
     User save (UserRegistrationDto userRegistrationDto);
+
 }
+
+
